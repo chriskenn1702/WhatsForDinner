5ea824c508cbf2a806e905c66df549a678aa769c
Christopher Kennedy <kennednu@bc.edu>
2023-02-13 17:11:28 -0500

    Initial Commit

---
+++ WhatsForDinner/WhatsForDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsForDinnerApp.swift
+//  WhatsForDinner
+//
+//  Created by Christopher Kennedy on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsForDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
